879801af979dbb405021aaaeacf515948a603a6b
Chris Bednarczyk <chris@tourradar.com>
2025-12-05 20:37:14 +0100

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 if [ ! -f ${HLDS_PATH}/cstrike/.installed ]; then
     echo "First run detected. Copying cstrike_base to cstrike..."
     mkdir -p ${HLDS_PATH}/cstrike
-    cp -r ${HLDS_PATH}/cstrike_base/* ${HLDS_PATH}/cstrike/
+    cp -rn ${HLDS_PATH}/cstrike_base/* ${HLDS_PATH}/cstrike/
     touch ${HLDS_PATH}/.installed
     echo "Installation complete."
 else
