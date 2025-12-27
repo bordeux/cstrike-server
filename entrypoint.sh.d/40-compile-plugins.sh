d88a0f096bee3167d63d2211f74464d1a169a94f
Chris Bednarczyk <chris@tourradar.com>
2025-12-28 00:24:07 +0100

    Add better output

--- entrypoint.sh.d/40-compile-plugins.sh
+++ entrypoint.sh.d/40-compile-plugins.sh
@@ -15,8 +15,22 @@ if [ "${AMXMODX_AUTOCOMPILE:-1}" = "1" ]; then
         for sma_file in *.sma; do
             if [ -f "$sma_file" ]; then
                 plugin_name="${sma_file%.sma}"
-                echo "Compiling: $sma_file -> ../plugins/${plugin_name}.amxx"
-                ./amxxpc "$sma_file" -o"../plugins/${plugin_name}.amxx"
+                echo -n "Compiling: $sma_file ... "
+
+                # Capture output and error
+                COMPILE_OUTPUT=$(./amxxpc "$sma_file" -o"../plugins/${plugin_name}.amxx" 2>&1)
+                COMPILE_EXIT_CODE=$?
+
+                if [ $COMPILE_EXIT_CODE -eq 0 ]; then
+                    echo "✓"
+                    ((COMPILE_COUNT++))
+                else
+                    echo "✗ FAILED"
+                    echo "--- Compilation output ---"
+                    echo "$COMPILE_OUTPUT"
+                    echo "-------------------------"
+                    ((FAILED_COUNT++))
+                fi
             fi
         done
 
