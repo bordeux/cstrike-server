d5548b0c2a9fee9c0a7924867d2f86995441452e
Chris Bednarczyk <chris@tourradar.com>
2025-12-29 19:11:30 +0100

    Small fix

--- bin/build-and-run.sh
+++ bin/build-and-run.sh
@@ -3,4 +3,4 @@
 set -e
 
 docker build --platform=linux/amd64 -t cstrike --no-cache .
-docker run -it  --platform=linux/amd64 --rm -p 26900:26900/udp -p 27020:27020/udp -p 27015:27015/udp -p 27015:27015 -p 8080:8080 cstrike
+docker run -it  --platform=linux/amd64 -e HLTV_ENABLE=1 --rm -p 26900:26900/udp -p 27020:27020/udp -p 27015:27015/udp -p 27015:27015 -p 8080:8080 cstrike
